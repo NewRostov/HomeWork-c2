02e1d85c541b9510fe3e91ee61b86f3f87c2ae1e
NewRostov <kim.sergey.rostov@gmail.com>
2023-03-14 17:13:44 +0100

    task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -3,10 +3,16 @@
 // 7 -> да
 // 1 -> нет
 
-string[] week = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
-Console.WriteLine("Введите цифру, обозначающую день недели");
-int n = Convert.ToInt32(Console.ReadLine());
-int i = n - 1;
-if ( n > 0 && n <= 5 ) {Console.WriteLine(week[i] + " не выходной день!");}
-if ( n >= 6 && n <= 7 ) {Console.WriteLine(week[i] + " выходной день!");}
-if ( n <= 0 || n > 7) {Console.WriteLine("Введите цифру, обозначающую день недели от 1 до 7");}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] week = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        Console.WriteLine("Введите цифру, обозначающую день недели");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int i = n - 1;
+        if (n > 0 && n <= 5) { Console.WriteLine(week[i] + " не выходной день!"); }
+        if (n >= 6 && n <= 7) { Console.WriteLine(week[i] + " выходной день!"); }
+        if (n <= 0 || n > 7) { Console.WriteLine("Введите цифру, обозначающую день недели от 1 до 7"); }
+    }
+}
